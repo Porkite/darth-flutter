094bc444f7b4b1ca42fe71d2f6377aac840090a4
mariusz0674 <83462478+mariusz0674@users.noreply.github.com>
2024-09-15 15:43:53 +0200

    rat fight part refactor

--- lib/rat-fight/game-widgets/rat-fight-service.dart
+++ lib/rat-fight/game-widgets/rat-fight-service.dart
@@ -1,17 +1,14 @@
 import 'dart:async';
 import 'dart:math';
-import 'package:darth_flutter/rat-fight/rat-fight-state.dart';
 import 'package:flutter/material.dart';
 import 'package:one_dollar_unistroke_recognizer/one_dollar_unistroke_recognizer.dart';
-
 import '../../service/game_manager.dart';
 import '../../service/items_manager.dart';
 import '../../service/model/equipment_state.dart';
+import '../rat-fight-state.dart';
 
 class RatFightService with ChangeNotifier {
-  Timer? _timer;
-  Timer? _attackTimer;
-  Timer? _givenDamageScoreTimer;
+  final List<Timer> _timers = [];
   double _leftPosition = 0;
   double _topPosition = 0;
   bool _showGivenDamage = false;
@@ -25,38 +22,41 @@ class RatFightService with ChangeNotifier {
   final List<Offset> _swipePath = [];
   bool _initialized = false;
   late BuildContext _context;
-  final StreamController<void> _blockGestureController = StreamController<void>.broadcast();
   late double scale;
   Map<String, Item> eqItems = {};
-  Stream<void> get  _blockGestureStream => _blockGestureController.stream;
+  final StreamController<void> _blockGestureController = StreamController<void>.broadcast();
 
+  // Getters
+  Stream<void> get blockGestureStream => _blockGestureController.stream;
   double get leftPosition => _leftPosition;
-
   double get topPosition => _topPosition;
-
   bool get showGivenDamage => _showGivenDamage;
-
   int get givenDamageScore => _givenDamageScore;
-
   int get getEnemyHealth => enemyHealth;
-
   List<Offset> get swipePath => List.unmodifiable(_swipePath);
-
   bool get isSwipeDetected => _swipeDetected;
-
   bool get isFlashing => _isFlashing;
 
   void initialize(BuildContext context) {
-    if (!_initialized) {
-      _context = context;
-      _moveToRandomPosition();
-      _initialized = true;
-      _startMovingEnemy();
-      _startAttackTimer();
-      double screenHeight = MediaQuery.of(_context).size.height;
-      scale = calculateScale(screenHeight);
-      eqItems = ItemsManager().getItemsAsItemsFromInventory(GameManager().getPlayerEquipment().getItems());
-    }
+    if (_initialized) return;
+
+    _context = context;
+    _moveToRandomPosition();
+    _initialized = true;
+    _startMovingEnemy();
+    _startAttackTimer();
+    _initializeScale();
+    eqItems = _loadEquipmentItems();
+  }
+
+  void _initializeScale() {
+    final screenHeight = MediaQuery.of(_context).size.height;
+    scale = calculateScale(screenHeight);
+  }
+
+  Map<String, Item> _loadEquipmentItems() {
+    final playerEquipment = GameManager().getPlayerEquipment().getItems();
+    return ItemsManager().getItemsAsItemsFromInventory(playerEquipment);
   }
 
   void _startMovingEnemy() {
@@ -64,67 +64,64 @@ class RatFightService with ChangeNotifier {
   }
 
   void _startAttackTimer() {
-    _attackTimer = Timer.periodic(const Duration(seconds: 3), (timer) {
+    final attackTimer = Timer.periodic(const Duration(seconds: 3), (_) {
       _triggerFlashing();
       _startBlockTimer();
     });
+    _timers.add(attackTimer);
   }
 
   void _startBlockTimer() {
     bool isBlocked = false;
-    _blockGestureStream.listen((_) {
-      isBlocked = true;
-    });
+    blockGestureStream.listen((_) => isBlocked = true);
 
-    Timer(const Duration(seconds: _timeForBlock), () {
-      if (!isBlocked) {
-        playerHealth -= 15;
-        if(playerHealth <= 0){
-          loseGame(_context);
-        }
-        notifyListeners();
-      }
-      isBlocked = false;
+    final blockTimer = Timer(const Duration(seconds: _timeForBlock), () {
+      if (!isBlocked) _applyPlayerDamage(15);
     });
+    _timers.add(blockTimer);
+  }
+
+  void _applyPlayerDamage(int damage) {
+    playerHealth = max(0, playerHealth - damage);
+    if (playerHealth <= 0) loseGame();
+    notifyListeners();
   }
 
   void _triggerFlashing() {
     _isFlashing = true;
     notifyListeners();
 
-    Timer(const Duration(seconds: _timeForBlock), () {
+    final flashingTimer = Timer(const Duration(seconds: _timeForBlock), () {
       _isFlashing = false;
       notifyListeners();
     });
+    _timers.add(flashingTimer);
   }
 
   void _setNextMove() {
-    Random random = Random();
-    int randomTime = 500 + random.nextInt(1500);
-    _timer?.cancel();
-    _timer = Timer(Duration(milliseconds: randomTime), () {
+    final randomTime = 500 + Random().nextInt(1500);
+    final moveTimer = Timer(Duration(milliseconds: randomTime), () {
       _moveToRandomPosition();
       _setNextMove();
     });
+    _timers.add(moveTimer);
   }
 
   void _moveToRandomPosition() {
-    Random random = Random();
-    double screenWidth = MediaQuery.of(_context).size.width;
-    double screenHeight = MediaQuery.of(_context).size.height;
+    final screenWidth = MediaQuery.of(_context).size.width;
+    final screenHeight = MediaQuery.of(_context).size.height;
+    final maxY = screenHeight * 2 / 3;
+    final minY = screenHeight * 1 / 8;
 
-    double maxY = screenHeight * 2 / 3;
-    double minY = screenHeight * 1 / 8;
-
-    _leftPosition = random.nextDouble() * (screenWidth - 150);
-    _topPosition = minY + random.nextDouble() * (maxY - minY - 50);
+    _leftPosition = Random().nextDouble() * (screenWidth - 150);
+    _topPosition = minY + Random().nextDouble() * (maxY - minY - 50);
     notifyListeners();
   }
 
   double calculateScale(double screenHeight) {
-    double minY = screenHeight * 1 / 8;
-    double maxY = screenHeight * 1 / 2;
-    double normalizedTop = (_topPosition - minY) / (maxY - minY);
+    final minY = screenHeight * 1 / 8;
+    final maxY = screenHeight * 1 / 2;
+    final normalizedTop = (_topPosition - minY) / (maxY - minY);
     return 1.0 / 3.0 + normalizedTop * (1.0 - 1.0 / 3.0);
   }
 
@@ -134,31 +131,38 @@ class RatFightService with ChangeNotifier {
     }
   }
 
-  void handleGesture(
-      List<Offset> points, RecognizedUnistroke? recognizedUnistroke) {
-    if (recognizedUnistroke != null) {
-      if (recognizedUnistroke.name == DefaultUnistrokeNames.circle) {
+  void handleGesture(RecognizedUnistroke? recognizedUnistroke) {
+    if (recognizedUnistroke == null) return;
+
+    switch (recognizedUnistroke.name) {
+      case DefaultUnistrokeNames.circle:
         _blockGestureController.add(null);
-      } else if (recognizedUnistroke.name == DefaultUnistrokeNames.triangle) {
+        break;
+      case DefaultUnistrokeNames.triangle:
         _applyDamage(20);
-      }
+        break;
+      default:
+        break;
     }
   }
 
   void _applyDamage(int damage) {
     enemyHealth = max(0, enemyHealth - damage);
-    if(enemyHealth <= 0){
-      winGame(_context);
-    }
+    if (enemyHealth <= 0) winGame();
+    _showGivenDamageAnimation(damage);
+  }
+
+  void _showGivenDamageAnimation(int damage) {
     _swipeDetected = true;
-    notifyListeners();
     _givenDamageScore = damage;
     _showGivenDamage = true;
-    _givenDamageScoreTimer?.cancel();
-    _givenDamageScoreTimer = Timer(Duration(seconds: 1), () {
+    notifyListeners();
+
+    final damageTimer = Timer(const Duration(seconds: 1), () {
       _showGivenDamage = false;
       notifyListeners();
     });
+    _timers.add(damageTimer);
   }
 
   void addSwipePath(Offset position) {
@@ -179,25 +183,25 @@ class RatFightService with ChangeNotifier {
         position.dy <= _topPosition + 150;
   }
 
-  void winGame(BuildContext context) {
-    Navigator.pop(context, RatFightState.WIN);
+  void winGame() {
+    Navigator.pop(_context, RatFightState.WIN);
   }
 
   void useEqItem(String itemIdentifier) {
     GameManager().getPlayerEquipment().removeItem(itemIdentifier);
-    eqItems = ItemsManager().getItemsAsItemsFromInventory(GameManager().getPlayerEquipment().getItems());
+    eqItems = _loadEquipmentItems();
     playerHealth += 50;
   }
 
-  void loseGame(BuildContext context) {
-    Navigator.pop(context, RatFightState.LOSE);
+  void loseGame() {
+    Navigator.pop(_context, RatFightState.LOSE);
   }
 
   @override
   void dispose() {
-    _attackTimer?.cancel();
-    _timer?.cancel();
-    _givenDamageScoreTimer?.cancel();
+    for (var timer in _timers) {
+      timer.cancel();
+    }
     _blockGestureController.close();
     super.dispose();
   }
--- lib/rat-fight/game-widgets/rat-fight-widget.dart
+++ lib/rat-fight/game-widgets/rat-fight-widget.dart
@@ -10,6 +10,9 @@ class RatFightWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
     return ChangeNotifierProvider<RatFightService>(
       create: (_) => RatFightService(),
       child: Consumer<RatFightService>(
@@ -17,176 +20,18 @@ class RatFightWidget extends StatelessWidget {
           WidgetsBinding.instance.addPostFrameCallback((_) {
             ratFightService.initialize(context);
           });
-
-          double screenHeight = MediaQuery.of(context).size.height;
           double scale = ratFightService.calculateScale(screenHeight);
-
           return GestureDetector(
-            onPanUpdate: (details) {
-              ratFightService.addSwipePath(details.localPosition);
-              if (!ratFightService.isSwipeDetected) {
-                ratFightService.handleSwipe(details.localPosition);
-              }
-            },
-            onPanEnd: (details) {
-              RecognizedUnistroke? recognizedUnistroke =
-              recognizeUnistroke(ratFightService.swipePath);
-              ratFightService.handleGesture(
-                  ratFightService.swipePath, recognizedUnistroke);
-              ratFightService.clearSwipePath();
-            },
+            onPanUpdate: (details) => _handlePanUpdate(details, ratFightService),
+            onPanEnd: (details) => _handlePanEnd(details, ratFightService),
             child: Stack(
               children: [
-                Container(
-                  decoration: const BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage('assets/images/rat-fighter/background.png'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                AnimatedPositioned(
-                  duration: const Duration(milliseconds: 500),
-                  curve: Curves.easeInOut,
-                  top: ratFightService.topPosition,
-                  left: ratFightService.leftPosition,
-                  child: AnimatedScale(
-                    scale: scale,
-                    duration: const Duration(milliseconds: 500),
-                    curve: Curves.easeInOut,
-                    child: Stack(
-                      children: [
-                        if (ratFightService.showGivenDamage)
-                          Text(
-                            '-${ratFightService.givenDamageScore} HP',
-                            style: const TextStyle(
-                              color: Colors.red,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        Positioned.fill(
-                          child: Center(
-                            child: Container(
-                              width: 180 * scale,
-                              height: 180 * scale,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: ratFightService.isFlashing
-                                    ? Colors.yellow.withOpacity(0.5)
-                                    : Colors.transparent,
-                              ),
-                            ),
-                          ),
-                        ),
-                        Image.asset(
-                          'assets/images/rat-fighter/rat.png',
-                          width: 150,
-                          height: 150,
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  left: (MediaQuery.of(context).size.width - 300) / 2,
-                  child: Image.asset(
-                    'assets/images/rat-fighter/player.png',
-                    width: 300,
-                    height: 300,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                Opacity(
-                    opacity: (1.0 - ratFightService.playerHealth / 100).clamp(0.0, 1.0),
-                    child: Container(
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/images/rat-fighter/blood_screen.png'),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    )),
-                Positioned(
-                  top: 20,
-                  left: MediaQuery.of(context).size.width / 2 - 125,
-                  child: Container(
-                    width: 250,
-                    height: 40,
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.brown, width: 3),
-                      borderRadius: BorderRadius.circular(20),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.black.withOpacity(0.5),
-                          spreadRadius: 3,
-                          blurRadius: 5,
-                          offset: Offset(0, 3),
-                        ),
-                      ],
-                      gradient: LinearGradient(
-                        colors: const [Colors.red, Colors.green],
-                        stops: [
-                          ratFightService.getEnemyHealth / 100,
-                          ratFightService.getEnemyHealth / 100
-                        ],
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight,
-                      ),
-                    ),
-                    child: Stack(
-                      children: [
-                        Positioned(
-                          left: 0,
-                          right: 0,
-                          top: 0,
-                          bottom: 0,
-                          child: AnimatedContainer(
-                            duration: const Duration(milliseconds: 500),
-                            curve: Curves.easeInOut,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              border: Border.all(
-                                color: ratFightService.isFlashing ? Colors.yellow : Colors.brown,
-                                width: 3,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 0,
-                  top: 20,
-                  bottom: 20,
-                  child: SizedBox(
-                    width: 50,
-                    child: ListView.builder(
-                      itemCount: ratFightService.eqItems.length,
-                      itemBuilder: (context, index) {
-                        String key = ratFightService.eqItems.keys.elementAt(index);
-                        Item item = ratFightService.eqItems[key]!;
-                        return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8.0),
-                            child: IconButton(
-                              icon: Image.asset(
-                                item.iconPath,
-                                width: 50,
-                                height: 50,
-                                fit: BoxFit.contain,
-                              ),
-                              onPressed: () {
-                                ratFightService.useEqItem(key);
-                              },
-                            )
-                        );
-                      },
-                    ),
-                  ),
-                ),
+                _buildBackgroundImage(),
+                _buildAnimatedRat(ratFightService, scale),
+                _buildPlayerImage(screenWidth),
+                _buildBloodOverlay(ratFightService),
+                _buildEnemyHealthBar(ratFightService, screenWidth),
+                _buildEquipmentList(ratFightService),
                 if (ratFightService.swipePath.isNotEmpty)
                   CustomPaint(
                     painter: GuestPointer(ratFightService.swipePath),
@@ -196,6 +41,183 @@ class RatFightWidget extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  Widget _buildBackgroundImage() {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage('assets/images/rat-fighter/background.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAnimatedRat(RatFightService ratFightService, double scale) {
+    return AnimatedPositioned(
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+      top: ratFightService.topPosition,
+      left: ratFightService.leftPosition,
+      child: AnimatedScale(
+        scale: scale,
+        duration: const Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+        child: Stack(
+          children: [
+            if (ratFightService.showGivenDamage)
+              Text(
+                '-${ratFightService.givenDamageScore} HP',
+                style: _damageTextStyle(),
+              ),
+            Positioned.fill(
+              child: Center(
+                child: Container(
+                  width: 180 * scale,
+                  height: 180 * scale,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: ratFightService.isFlashing
+                        ? Colors.yellow.withOpacity(0.5)
+                        : Colors.transparent,
+                  ),
+                ),
+              ),
+            ),
+            Image.asset(
+              'assets/images/rat-fighter/rat.png',
+              width: 150,
+              height: 150,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildPlayerImage(double screenWidth) {
+    return Positioned(
+      bottom: 0,
+      left: (screenWidth - 300) / 2,
+      child: Image.asset(
+        'assets/images/rat-fighter/player.png',
+        width: 300,
+        height: 300,
+        fit: BoxFit.fill,
+      ),
+    );
+  }
+
+  Widget _buildBloodOverlay(RatFightService ratFightService) {
+    return Opacity(
+      opacity: (1.0 - ratFightService.playerHealth / 100).clamp(0.0, 1.0),
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/rat-fighter/blood_screen.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEnemyHealthBar(RatFightService ratFightService, double screenWidth) {
+    return Positioned(
+      top: 20,
+      left: screenWidth / 2 - 125,
+      child: Container(
+        width: 250,
+        height: 40,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.brown, width: 3),
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.5),
+              spreadRadius: 3,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+          gradient: LinearGradient(
+            colors: const [Colors.red, Colors.green],
+            stops: [
+              ratFightService.getEnemyHealth / 100,
+              ratFightService.getEnemyHealth / 100
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+        ),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          curve: Curves.easeInOut,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            border: Border.all(
+              color: ratFightService.isFlashing ? Colors.yellow : Colors.brown,
+              width: 3,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEquipmentList(RatFightService ratFightService) {
+    return Positioned(
+      left: 0,
+      top: 20,
+      bottom: 20,
+      child: SizedBox(
+        width: 50,
+        child: ListView.builder(
+          itemCount: ratFightService.eqItems.length,
+          itemBuilder: (context, index) {
+            String key = ratFightService.eqItems.keys.elementAt(index);
+            Item item = ratFightService.eqItems[key]!;
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: IconButton(
+                icon: Image.asset(
+                  item.iconPath,
+                  width: 50,
+                  height: 50,
+                  fit: BoxFit.contain,
+                ),
+                onPressed: () {
+                  ratFightService.useEqItem(key);
+                },
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  void _handlePanUpdate(DragUpdateDetails details, RatFightService ratFightService) {
+    ratFightService.addSwipePath(details.localPosition);
+    if (!ratFightService.isSwipeDetected) {
+      ratFightService.handleSwipe(details.localPosition);
+    }
+  }
+
+  void _handlePanEnd(DragEndDetails details, RatFightService ratFightService) {
+    RecognizedUnistroke? recognizedUnistroke =
+    recognizeUnistroke(ratFightService.swipePath);
+    ratFightService.handleGesture(recognizedUnistroke);
+    ratFightService.clearSwipePath();
+  }
+
+  TextStyle _damageTextStyle() {
+    return const TextStyle(
+      color: Colors.red,
+      fontSize: 14,
+      fontWeight: FontWeight.bold,
     );
   }
 }
